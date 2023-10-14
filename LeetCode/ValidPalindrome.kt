e504ce8c8ed94d00b1c1c167abb24a9af4a204ac
Martin Wiechmann <martin.wiechmann.office@gmail.com>
2023-10-14 21:03:05 +0200

    Added ValidPalindrome solution

--- LeetCode/ValidPalindrome.kt
+++ LeetCode/ValidPalindrome.kt
@@ -1,5 +1,12 @@
 class ValidPalindrome {
-    fun isPalindrome(s: String): Boolean {
-        // TBD
+    fun isPalindrom(s: String): Boolean {
+        val sOnlyLetters = s.lowercase().filter { it.isLetterOrDigit() }
+        val checkUntil = sOnlyLetters.length / 2
+
+        sOnlyLetters.forEachIndexed { index, c ->
+            if (index > checkUntil) return true
+            if (c != sOnlyLetters[sOnlyLetters.length - 1 - index]) return false
+        }
+        return true
     }
 }
