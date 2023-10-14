2169932572b6d22918184eb667b8f417b3c2c15b
Martin Wiechmann <martin.wiechmann.office@gmail.com>
2023-10-14 21:58:38 +0200

    Added comment for solution 2

--- LeetCode/ValidPalindrome.kt
+++ LeetCode/ValidPalindrome.kt
@@ -11,6 +11,10 @@ class ValidPalindrome {
         return true
     }
 
+    // Found a pointer suggestion online the rest is self coded
+    // This is more memory efficient and according to the leetcode tests has a better runtime.
+    // But readability is far worse so in a real project I would most of the time prefer version 1
+    // Of course this method could be refactored, but the simplicity of method 1 makes this superfluous here
     fun isPalindrom2(s: String): Boolean {
         var leftPointer = 0
         var leftPointerIsAlphanumeric = true
