1935b1e323881529085df0d360d32f5d71f65165
Martin Wiechmann <martin.wiechmann.office@gmail.com>
2023-10-15 23:39:13 +0200

    Added first solution for BestTimeToBuyAndSellStock

--- LeetCode/BestTimeToBuyAndSellStock.kt
+++ LeetCode/BestTimeToBuyAndSellStock.kt
@@ -1,3 +1,19 @@
 class BestTimeToBuyAndSellStock {
-    //TBD
+
+    // First version jusr stupid brute force.
+    fun maxProfit(prices: IntArray): Int {
+        var currentMaxProfit = 0
+
+        for ((index, item) in prices.withIndex()) {
+            val currentList = prices.drop(index + 1)
+            currentList.forEach{
+                val tempProfit = it - item
+                if (tempProfit > currentMaxProfit) currentMaxProfit = tempProfit
+            }
+        }
+
+        return currentMaxProfit
+    }
+
+    // Test if multithreading for searching the sublists brings any benefit here(Good opportunity to practice multi Threading a bti ;) )
 }
