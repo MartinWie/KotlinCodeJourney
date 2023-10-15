94c2478832fa9b5e517ea116d70d8d744bf41fa5
Martin Wiechmann <martin.wiechmann.office@gmail.com>
2023-10-15 23:13:55 +0200

    Added solution 3

--- LeetCode/ValidPalindrome.kt
+++ LeetCode/ValidPalindrome.kt
@@ -13,8 +13,6 @@ class ValidPalindrome {
 
     // Found a pointer suggestion online the rest is self coded
     // This is more memory efficient and according to the leetcode tests has a better runtime.
-    // But readability is far worse so in a real project I would most of the time prefer version 1
-    // Of course this method could be refactored, but the simplicity of method 1 makes this superfluous here
     fun isPalindrom2(s: String): Boolean {
         var leftPointer = 0
         var leftPointerIsAlphanumeric = true
@@ -47,6 +45,30 @@ class ValidPalindrome {
 
             leftPointerIsAlphanumeric = true
             rightPointerIsAlphanumeric = true
+        }
+
+        return true
+    }
+
+    fun isPalindrom3(s: String): Boolean {
+        var leftPointer = 0
+        var rightPointer = s.length - 1 // Used for the index of the string s (index starting at 0)
+
+        while (leftPointer < rightPointer) {
+            if (!s[leftPointer].isLetterOrDigit()) {
+                leftPointer++
+                continue
+            }
+
+            if (!s[rightPointer].isLetterOrDigit()) {
+                rightPointer--
+                continue
+            }
+
+            if (s[leftPointer].lowercase() != s[rightPointer].lowercase()) return false
+
+            leftPointer++
+            rightPointer--
         }
 
         return true
