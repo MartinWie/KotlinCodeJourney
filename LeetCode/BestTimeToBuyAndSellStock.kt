fb0c64d439e194e3b1da72625e23c1f1a78ec83c
Martin Wiechmann <martin.wiechmann.office@gmail.com>
2023-10-16 21:44:05 +0200

    Added another solution slightly better than the pure brute force solution

--- LeetCode/BestTimeToBuyAndSellStock.kt
+++ LeetCode/BestTimeToBuyAndSellStock.kt
@@ -15,5 +15,24 @@ class BestTimeToBuyAndSellStock {
         return currentMaxProfit
     }
 
+    fun maxProfit2(prices: IntArray): Int {
+        var currentMaxProfit = 0
+        var currentHighestNumber: Int? = null
+
+        for ((index, item) in prices.withIndex()) {
+            if (item >= (currentHighestNumber ?: (item + 1))) continue
+            val currentList = prices.drop(index + 1)
+            currentList.forEach {
+                val tempProfit = it - item
+                if (tempProfit > currentMaxProfit) {
+                    currentMaxProfit = tempProfit
+                    currentHighestNumber = item
+                }
+            }
+        }
+
+        return currentMaxProfit
+    }
+
     // Test if multithreading for searching the sublists brings any benefit here(Good opportunity to practice multi Threading a bti ;) )
 }
