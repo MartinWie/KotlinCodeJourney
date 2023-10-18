2a2c33a3a6684970bab69dfd84b5c3187dc9eb30
Martin Wiechmann <martin.wiechmann.office@gmail.com>
2023-10-18 22:24:35 +0200

    Added first incomplete solution, case "{[]}" is not working rn.

--- LeetCode/ValidParentheses.kt
+++ LeetCode/ValidParentheses.kt
@@ -1,5 +1,24 @@
 class ValidParentheses {
     fun isValid(s: String): Boolean {
-        // TBD
+        var lastRound: Char? = null
+        var lastSquare: Char? = null
+        var lastCurly: Char? = null
+
+        for (char in s) {
+            when (char) {
+                '(' -> if (lastRound == null) lastRound = char else return false
+                ')' -> if (lastRound == '(' && lastSquare == null && lastCurly == null) lastRound = null else return false
+
+                '[' -> if (lastSquare == null) lastSquare = char else return false
+                ']' -> if (lastSquare == '[' && lastRound == null && lastCurly == null) lastSquare = null else return false
+
+                '{' -> if (lastCurly == null) lastCurly = char else return false
+                '}' -> if (lastCurly == '{' && lastRound == null && lastSquare == null) lastCurly = null else return false
+            }
+        }
+
+        return true
     }
+
+    // TODO: fix case "{[]}" -> true in next version
 }
