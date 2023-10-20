042fa6613caa3dca16ed560935375aad01df910d
Martin Wiechmann <martin.wiechmann.office@gmail.com>
2023-10-21 00:11:50 +0200

    Added gradle to this project, later I will add test and potentially logging this will make working with the dependencies simpler

---
+++ build.gradle.kts
@@ -0,0 +1,19 @@
+val kotlin_version: String by project
+val logback_version: String by project
+
+plugins {
+    id("org.jetbrains.kotlin.jvm") version "1.9.20-Beta"
+    id("org.jetbrains.kotlin.plugin.serialization") version "1.9.10"
+}
+
+group = "de.mw"
+version = "0.0.1"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation("ch.qos.logback:logback-classic:$logback_version")
+    testImplementation("org.jetbrains.kotlin:kotlin-test-junit:$kotlin_version")
+}
