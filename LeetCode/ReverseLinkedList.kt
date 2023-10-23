521fb2487c14c6398180c69eab86a4685873543e
Martin Wiechmann <martin.wiechmann.office@gmail.com>
2023-10-23 23:22:58 +0200

    Added ReverseLinkedList frame

---
+++ LeetCode/ReverseLinkedList.kt
@@ -0,0 +1,17 @@
+import kotlin.test.Test
+
+class ReverseLinkedList {
+    class ListNode(var `val`: Int) {
+        var next: ListNode? = null
+    }
+    class Solution {
+        fun reverseList(head: ListNode?): ListNode? {
+
+        }
+    }
+
+    @Test
+    fun test() {
+
+    }
+}
