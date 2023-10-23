98f042a421bebf38c1fc80af8159ef39e690f331
Martin Wiechmann <martin.wiechmann.office@gmail.com>
2023-10-23 23:41:26 +0200

    prepared ReverseLinkedList for implementation

--- LeetCode/ReverseLinkedList.kt
+++ LeetCode/ReverseLinkedList.kt
@@ -1,17 +1,38 @@
 import kotlin.test.Test
+import kotlin.test.assertEquals
 
 class ReverseLinkedList {
     class ListNode(var `val`: Int) {
         var next: ListNode? = null
     }
-    class Solution {
-        fun reverseList(head: ListNode?): ListNode? {
 
-        }
+    fun reverseList(head: ListNode?): ListNode? {
+        return head
     }
 
     @Test
     fun test() {
+        val n1 = ListNode(1)
+        val n2 = ListNode(2)
+        n1.next = n2
 
+        val solution = getValListFromNodes(reverseList(n1))
+
+        n1.next = null
+        n2.next = n1
+        val shouldBe = getValListFromNodes(n2)
+
+        assertEquals(shouldBe.toString(), solution.toString())
+    }
+
+    private fun getValListFromNodes(head: ListNode?): MutableList<Int> {
+        val list = mutableListOf<Int>()
+        var currentNode: ListNode? = head
+        while (currentNode != null) {
+            list.add(currentNode.`val`)
+            currentNode = currentNode.next
+        }
+
+        return list
     }
 }
