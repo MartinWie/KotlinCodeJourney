d98d87d900c883649dcb1a0cf9f4b1460a57f684
MartinWie <donotsuspend@googlegroups.com>
2023-10-31 23:59:31 +0100

    save current state

--- LeetCode/MergeTwoSortedLists.kt
+++ LeetCode/MergeTwoSortedLists.kt
@@ -41,7 +41,7 @@ class MergeTwoSortedLists {
         return firstNode
     }
 
-    private fun appendNodeToEndOfList(head: ListNode?, append: ListNode): ListNode? {
+    private fun appendNodeToEndOfList(head: ListNode?, append: ListNode): ListNode {
         append.next = null
         if (head == null) return append
 
@@ -67,6 +67,57 @@ class MergeTwoSortedLists {
         val solutionList = nodesToList(solutionNodes)
 
         a2.next = b2
+        val expectedList = nodesToList(a1)
+
+        assertEquals(expectedList.toString(), solutionList.toString())
+    }
+
+    private fun mergeTwoLists2(list1: ListNode?, list2: ListNode?): ListNode? {
+        val placeholderNode = ListNode(0)
+        var currentLastNode = placeholderNode
+        var list1Pointer = list1
+        var list2Pointer = list2
+
+        while (list1Pointer != null || list2Pointer != null) {
+            when {
+                (list1Pointer == null) -> {
+                    currentLastNode.next = list2Pointer.next
+                }
+                (list2Pointer == null) -> {
+                    currentLastNode.next = list1Pointer.next
+                }
+                (list1Pointer.`val` <= list2Pointer.`val`) -> {
+                    currentLastNode.next = list1Pointer
+                    list1Pointer = list1Pointer.next
+                }
+                else -> {
+                    currentLastNode.next = list2Pointer
+                    list2Pointer = list2Pointer.next
+                }
+            }
+            currentLastNode = currentLastNode.next!!
+        }
+        return placeholderNode.next
+    }
+
+    @Test
+    fun testMerge2() {
+        val a1 = ListNode(1)
+        val a2 = ListNode(2)
+        val a3 = ListNode(3)
+        val b1 = ListNode(9)
+        val b2 = ListNode(2)
+
+        a1.next = a2
+        a2.next = a3
+        b2.next = b1
+        val solutionNodes = mergeTwoLists2(a1, b2)
+        val solutionList = nodesToList(solutionNodes)
+
+        a1.next = a2
+        a2.next = b2
+        b2.next = a3
+        a3.next = b1
         val expectedList = nodesToList(a1)
 
         assertEquals(expectedList.toString(), solutionList.toString())
