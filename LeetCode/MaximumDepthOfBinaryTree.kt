0fffc953383f7ac371bfd29762aca095e64cf147
MartinWie <donotsuspend@googlegroups.com>
2023-11-06 23:36:06 +0100

    Added solution without recursion

--- LeetCode/MaximumDepthOfBinaryTree.kt
+++ LeetCode/MaximumDepthOfBinaryTree.kt
@@ -1,3 +1,5 @@
+import java.util.Stack
+
 class MaximumDepthOfBinaryTree {
 
     fun maxDepth(root: TreeNode?): Int {
@@ -7,6 +9,25 @@ class MaximumDepthOfBinaryTree {
         val leftNumber = 1 + maxDepth(root.left)
 
         return if (rightNumber >= leftNumber) rightNumber else leftNumber
+    }
+
+    fun maxDepth2(root: TreeNode?): Int {
+        if (root == null) return 0
+
+        val stack = Stack<Pair<TreeNode, Int>>()
+        var currentMaxDepth = 1
+
+        stack.push(Pair(root, currentMaxDepth))
+
+        while (stack.isNotEmpty()) {
+            val (node, depth) = stack.pop()
+            currentMaxDepth = maxOf(currentMaxDepth, depth)
+
+            node.right?.let { stack.push(Pair(it, depth + 1)) }
+            node.left?.let { stack.push(Pair(it, depth + 1)) }
+        }
+
+        return currentMaxDepth
     }
 
     class TreeNode(var `val`: Int) {
