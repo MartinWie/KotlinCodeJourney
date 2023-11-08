1f4d2a29dfc5ce856a4e4799a39d3327bb1be961
MartinWie <donotsuspend@googlegroups.com>
2023-11-08 21:48:23 +0100

    Added frame for DiameterOfBinaryTree

---
+++ LeetCode/DiameterOfBinaryTree.kt
@@ -0,0 +1,10 @@
+class DiameterOfBinaryTree {
+    fun diameterOfBinaryTree(root: TreeNode?): Int {
+        return 1 // TBD
+    }
+}
+
+class TreeNode(var `val`: Int) {
+    var left: TreeNode? = null
+    var right: TreeNode? = null
+}
