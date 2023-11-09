a0b008a103475fa11f11c31582e30ab4f34559b9
MartinWie <donotsuspend@googlegroups.com>
2023-11-09 22:29:28 +0100

    Added first draft, but this fails for the given tree -> [4,2,null,1,3]

--- LeetCode/DiameterOfBinaryTree.kt
+++ LeetCode/DiameterOfBinaryTree.kt
@@ -1,6 +1,30 @@
+import java.util.Stack
+
 class DiameterOfBinaryTree {
     fun diameterOfBinaryTree(root: TreeNode?): Int {
-        return 1 // TBD
+        if (root == null) return 0
+
+        var currentMaxDiameterLeftBranch = 0
+        var currentMaxDiameterRightBranch = 0
+        val stack = Stack<Triple<TreeNode, Int, Int>>()
+
+        stack.push(Triple(root, currentMaxDiameterLeftBranch, currentMaxDiameterRightBranch))
+
+        while (stack.isNotEmpty()) {
+            val (currentNode, left, right) = stack.pop()
+
+            if (currentNode.left != null) {
+                currentMaxDiameterLeftBranch = left + 1
+                stack.push(Triple(currentNode.left!!, left + 1, right))
+            }
+
+            if (currentNode.right != null) {
+                currentMaxDiameterRightBranch = right + 1
+                stack.push(Triple(currentNode.right!!, left, right + 1))
+            }
+        }
+
+        return currentMaxDiameterLeftBranch + currentMaxDiameterRightBranch
     }
 }
 
