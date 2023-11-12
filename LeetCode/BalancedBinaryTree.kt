e097b1dc473b87e1998ee56fdeac156eac04f8c0
MartinWie <donotsuspend@googlegroups.com>
2023-11-12 16:58:54 +0100

    First flawed implementation this fails for root =[1,2,2,3,null,null,3,4,null,null,4]

--- LeetCode/BalancedBinaryTree.kt
+++ LeetCode/BalancedBinaryTree.kt
@@ -1,10 +1,24 @@
 class BalancedBinaryTree {
     fun isBalanced(root: TreeNode?): Boolean {
-        // TBD
-    }
-}
+        if (root == null) return true
 
-class TreeNode(var `val`: Int) {
-    var left: TreeNode? = null
-    var right: TreeNode? = null
+        val lMaxDepth = maxDepth(root.left)
+        val rMaxDepth = maxDepth(root.right)
+
+        return (lMaxDepth - rMaxDepth) in listOf(-1, 0, 1)
+    }
+
+    private fun maxDepth(node: TreeNode?): Int {
+        if (node == null) return 0
+
+        val left = 1 + maxDepth(node.left)
+        val right = 1 + maxDepth(node.right)
+
+        return maxOf(left, right)
+    }
+
+    class TreeNode(var `val`: Int) {
+        var left: TreeNode? = null
+        var right: TreeNode? = null
+    }
 }
