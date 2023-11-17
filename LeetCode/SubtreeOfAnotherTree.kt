e018be0082011c733361fba84b05f5c3ce480752
MartinWie <donotsuspend@googlegroups.com>
2023-11-17 20:30:02 +0100

    Checked in first broken solution fails for 1,1 to 1

--- LeetCode/SubtreeOfAnotherTree.kt
+++ LeetCode/SubtreeOfAnotherTree.kt
@@ -1,7 +1,11 @@
 class SubtreeOfAnotherTree {
     fun isSubtree(root: TreeNode?, subRoot: TreeNode?): Boolean {
-        // TODO: implement
-        return false
+        if (root == null && subRoot == null) return true
+        if (root == null || subRoot == null) return false
+        if (root.`val` == subRoot.`val`) {
+            return isSubtree(root.left, subRoot.left) && isSubtree(root.right, subRoot.right)
+        }
+        return isSubtree(root.left, subRoot) || isSubtree(root.right, subRoot)
     }
 
     class TreeNode(var `val`: Int) {
