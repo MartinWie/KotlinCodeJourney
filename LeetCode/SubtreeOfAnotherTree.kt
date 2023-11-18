eca219590de6b16be4e6dd4d9c5b0e656b8e6aca
MartinWie <donotsuspend@googlegroups.com>
2023-11-18 21:58:04 +0100

    Added recursive solution

--- LeetCode/SubtreeOfAnotherTree.kt
+++ LeetCode/SubtreeOfAnotherTree.kt
@@ -27,14 +27,20 @@ class SubtreeOfAnotherTree {
         return false
     }
 
+    fun isSubtree2(root: TreeNode?, subRoot: TreeNode?): Boolean {
+        if(subRoot == null) return true
+        if(root == null) return false
+        if(isSameTree(root, subRoot)) return true
+
+        return (isSubtree2(root.left, subRoot) || isSubtree2(root.right, subRoot))
+    }
+
     private fun isSameTree(tree1: TreeNode?, tree2: TreeNode?): Boolean {
         if (tree1 == null && tree2 == null) return true
         if (tree1 == null || tree2 == null) return false
+        if (tree1.`val` != tree2.`val`) return false
 
-        if (tree1.`val` == tree2.`val`) {
-            return isSameTree(tree1.left, tree2.left) && isSameTree(tree1.right, tree2.right)
-        }
-        return false
+        return isSameTree(tree1.left, tree2.left) && isSameTree(tree1.right, tree2.right)
     }
 
     class TreeNode(var `val`: Int) {
