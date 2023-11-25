97d5d8887581df2549e06389fa061963519b3054
MartinWie <donotsuspend@googlegroups.com>
2023-11-25 19:01:01 +0100

    Added first brute force solution

--- LeetCode/ContainerWithMostWater.kt
+++ LeetCode/ContainerWithMostWater.kt
@@ -1,5 +1,15 @@
+import kotlin.math.abs
+
 class ContainerWithMostWater {
     fun maxArea(height: IntArray): Int {
-        // TBD
+        if (height.size <= 1) return 0
+        var maxArea = 0
+        for (outerIndex in height.indices) {
+            for (innerIndex in height.indices) {
+                val currentArea = minOf(height[outerIndex], height[innerIndex]) * abs(innerIndex-outerIndex)
+                if ( currentArea > maxArea) maxArea = currentArea
+            }
+        }
+        return maxArea
     }
 }
