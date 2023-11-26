27d13977da9fbd1cddca9a00472034b74814dfbe
MartinWie <donotsuspend@googlegroups.com>
2023-11-26 19:05:20 +0100

    Optimized the solution + added a test

--- LeetCode/ContainerWithMostWater.kt
+++ LeetCode/ContainerWithMostWater.kt
@@ -1,4 +1,6 @@
+import org.junit.Test
 import kotlin.math.abs
+import kotlin.test.assertEquals
 
 class ContainerWithMostWater {
     fun maxArea(height: IntArray): Int {
@@ -6,10 +8,30 @@ class ContainerWithMostWater {
         var maxArea = 0
         for (outerIndex in height.indices) {
             for (innerIndex in height.indices) {
-                val currentArea = minOf(height[outerIndex], height[innerIndex]) * abs(innerIndex-outerIndex)
-                if ( currentArea > maxArea) maxArea = currentArea
+                val currentArea = minOf(height[outerIndex], height[innerIndex]) * abs(innerIndex - outerIndex)
+                if (currentArea > maxArea) maxArea = currentArea
             }
         }
         return maxArea
+    }
+
+    fun maxArea2(height: IntArray): Int {
+        if (height.size <= 1) return 0
+        var maxArea = 0
+        var leftPointer = 0
+        var rightPointer = height.size - 1
+        while (leftPointer < rightPointer) {
+            val currentArea = minOf(height[leftPointer], height[rightPointer]) * (rightPointer - leftPointer)
+            if (currentArea > maxArea) maxArea = currentArea
+
+            if (height[leftPointer] >= height[rightPointer]) rightPointer-- else leftPointer++
+        }
+        return maxArea
+    }
+
+    @Test
+    fun test() {
+        assertEquals(49, maxArea(intArrayOf(1, 8, 6, 2, 5, 4, 8, 3, 7)))
+        assertEquals(49, maxArea2(intArrayOf(1, 8, 6, 2, 5, 4, 8, 3, 7)))
     }
 }
