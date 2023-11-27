e170ef13a3fed9be6e2413837478d01c6b82214e
MartinWie <donotsuspend@googlegroups.com>
2023-11-27 22:01:17 +0100

    Added frame, test and first broken version

---
+++ LeetCode/LongestSubstringWithoutRepeatingCharacters.kt
@@ -0,0 +1,29 @@
+import org.junit.Test
+import kotlin.test.assertEquals
+
+class LongestSubstringWithoutRepeatingCharacters {
+    fun lengthOfLongestSubstring(s: String): Int {
+        if (s.isEmpty()) return 0
+        if (s.length == 1) return 1
+        var maxStreak = 0
+        for (outerLetter in s) {
+            val hashMap = HashMap<Char, String>()
+            var currentStreak = 0
+            for (innerLetter in s) {
+                if(hashMap.containsKey(innerLetter)) {
+                    break
+                } else {
+                    hashMap[innerLetter] = ""
+                    currentStreak++
+                }
+            }
+            if(currentStreak > maxStreak) maxStreak = currentStreak
+        }
+        return maxStreak
+    }
+
+    @Test
+    fun test() {
+        assertEquals(3,lengthOfLongestSubstring("pwwkew"))
+    }
+}
