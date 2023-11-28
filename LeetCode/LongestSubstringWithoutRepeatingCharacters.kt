3f6b6ced4c9dfcb2001eaa0f5fdeb6836a90d3ab
MartinWie <donotsuspend@googlegroups.com>
2023-11-28 23:24:29 +0100

    new version, still fails for "dvdf" (cases where the longest substring begins before a shorter one had ended)

--- LeetCode/LongestSubstringWithoutRepeatingCharacters.kt
+++ LeetCode/LongestSubstringWithoutRepeatingCharacters.kt
@@ -5,16 +5,18 @@ class LongestSubstringWithoutRepeatingCharacters {
     private fun lengthOfLongestSubstring(s: String): Int {
         if (s.isEmpty()) return 0
         if (s.length == 1) return 1
+        var currentString = s
         var maxStreak = 0
-        for (outerLetter in s) {
+        while (currentString.length > 0) {
             val hashMap = HashMap<Char, String>()
             var currentStreak = 0
-            for (innerLetter in s) {
+            for (innerLetter in currentString) {
                 if (hashMap.containsKey(innerLetter)) {
                     break
                 } else {
                     hashMap[innerLetter] = ""
                     currentStreak++
+                    currentString = currentString.drop(1)
                 }
             }
             if (currentStreak > maxStreak) maxStreak = currentStreak
@@ -24,6 +26,9 @@ class LongestSubstringWithoutRepeatingCharacters {
 
     @Test
     fun test() {
+        assertEquals(3, lengthOfLongestSubstring("dvdf"))
+        assertEquals(2, lengthOfLongestSubstring("aab"))
         assertEquals(3, lengthOfLongestSubstring("pwwkew"))
+        assertEquals(3, lengthOfLongestSubstring("abcabcbb"))
     }
 }
