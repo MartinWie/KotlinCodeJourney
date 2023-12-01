d01387c6848bf492a29886ba0d85f20c896c1315
MartinWie <donotsuspend@googlegroups.com>
2023-12-01 23:59:48 +0100

    Solution 1

--- AdventOfCode/Challenge2023Day01.kt
+++ AdventOfCode/Challenge2023Day01.kt
@@ -1,10 +1,85 @@
 import org.junit.Test
+import java.io.File
+import java.lang.IllegalArgumentException
 import kotlin.test.assertTrue
 
 class Challenge2023Day01 {
 
+    private fun solve1(): Int {
+        val lines = File("./AdventOfCode/Data/Day01-1.txt").bufferedReader().readLines()
+        val numberList = mutableListOf<Int>()
+        for (line in lines) {
+            val currentNumber = "${getFirstDigitAsChar(line)}${getLastDigitAsChar(line)}"
+            numberList.add(currentNumber.toInt())
+        }
+        return numberList.sum()
+    }
+
+    private fun getFirstDigitAsChar(text: String): Char {
+        for (char in text) {
+            if (char.isDigit()) return char
+        }
+        throw IllegalArgumentException("No digit in text: $text")
+    }
+
+    private fun getLastDigitAsChar(text: String): Char {
+        val reversedText = text.reversed()
+        for (char in reversedText) {
+            if (char.isDigit()) return char
+        }
+        throw IllegalArgumentException("No digit in text: $text")
+    }
+
+    private fun solve2(): Int {
+        val lines = File("./AdventOfCode/Data/Day01-2-test.txt").bufferedReader().readLines()
+        val numberList = mutableListOf<Int>()
+        for (line in lines) {
+            val currentNumber = "${getFirstDigit(line)}${getLastDigit(line)}"
+            numberList.add(currentNumber.toInt())
+        }
+        return numberList.sum()
+    }
+
+    private fun getFirstDigit(text: String): Int {
+        val charList = mutableListOf<Char>()
+        for (char in text) {
+            charList.add(char)
+            if (charList.size > 2) {
+                val stringBeforeNumber = charList.joinToString(separator = "")
+                for (numberString in numberStringList) {
+                    if (stringBeforeNumber.contains(numberString.key)) return numberString.value
+                }
+            }
+
+            if (char.isDigit()) {
+                return char.digitToInt()
+            }
+        }
+        throw IllegalArgumentException("No digit in text: $text")
+    }
+
+    private fun getLastDigit(text: String): Int {
+        val reversedText = text.reversed()
+        val charList = mutableListOf<Char>()
+        for (char in reversedText) {
+            charList.add(char)
+            if (char.isDigit()) {
+                val stringBeforeNumber = charList.joinToString(separator = "").reversed()
+                for (numberString in numberStringList) {
+                    if (stringBeforeNumber.contains(numberString.key)) return numberString.value
+                }
+                return char.digitToInt()
+            }
+        }
+        throw IllegalArgumentException("No digit in text: $text")
+    }
+
+    private val numberStringList = mutableMapOf("one" to 1, "two" to 2, "three" to 3, "four" to 4, "five" to 5, "six" to 6, "seven" to 7, "eight" to 8, "nine" to 9)
+
     @Test
     fun test() {
+        println("Solution 1: ${solve1()}")
+        println("Solution 2: ${solve2()}")
         assertTrue { true }
     }
 }
