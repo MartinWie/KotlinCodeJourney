e7b22f4183e63fbb9510782feb70e453f6f17b9c
MartinWie <donotsuspend@googlegroups.com>
2023-12-06 21:06:16 +0100

    Solved Day 4

--- AdventOfCode/Challenge2023Day04.kt
+++ AdventOfCode/Challenge2023Day04.kt
@@ -1,22 +1,48 @@
 import org.junit.Test
+import java.io.File
 import kotlin.test.assertEquals
 
 class Challenge2023Day04 {
 
-    private fun solve1(): Int {
+    private fun solve1(lines: List<String>): Int {
         val numberList = mutableListOf<Int>()
+        val games = lines.map { lineToGameNumbers(it) }
+
+        games.forEach { game ->
+            var currentWinCounter = 0
+            game.second.forEach { ownNumber ->
+                if (game.first.contains(ownNumber)) {
+                    if (currentWinCounter == 0) {
+                        currentWinCounter++
+                    } else {
+                        currentWinCounter *= 2
+                    }
+                }
+            }
+            numberList.add(currentWinCounter)
+        }
 
         return numberList.sum()
     }
 
+    private fun lineToGameNumbers(line: String): Pair<HashSet<Int>, List<Int>> {
+        val rawGame = line.replace("  ", " ").split(":")[1]
+        val winningNumbersHashSet = rawGame.split("|")[0].trim().split(" ").map { it.toInt() }.toHashSet()
+        val ownNumbers = rawGame.split("|")[1].trim().split(" ").map { it.toInt() }
+
+        return winningNumbersHashSet to ownNumbers
+    }
+
     @Test
     fun test() {
-        val exampleSolution1 = solve1()
+        val lines = File("./AdventOfCode/Data/Day04-1-Test-Data.txt").bufferedReader().readLines()
+        val exampleSolution1 = solve1(lines)
         println("Example solution 1: $exampleSolution1")
-        assertEquals(4361, exampleSolution1)
+        assertEquals(13, exampleSolution1)
 
-        val solution1 = solve1()
+        val realLines = File("./AdventOfCode/Data/Day04-1-Data.txt").bufferedReader().readLines()
+        val solution1 = solve1(realLines)
         println("Solution 1: $solution1")
-        assertEquals(546563, solution1)
+        assertEquals(26426, solution1)
     }
 }
