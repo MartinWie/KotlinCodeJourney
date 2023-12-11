365f0e0f79f3b350a315122462ab6de09ab53001
MartinWie <donotsuspend@googlegroups.com>
2023-12-11 23:34:01 +0100

    Refactor Challenge2023Day06.kt to solve problem correctly
    The function solve1 has been significantly updated to accurately calculate the winning amounts based on the given data. The test assertions have been amended to reflect the correct results as well. Additionally, the getNumbers function has been introduced to parse integers from the data lines. While this solution works for the provided inputs, it can potentially run into a java.lang.OutOfMemoryError for larger datasets.

--- AdventOfCode/Challenge2023Day06.kt
+++ AdventOfCode/Challenge2023Day06.kt
@@ -4,7 +4,26 @@ import kotlin.test.assertEquals
 
 class Challenge2023Day06 {
     private fun solve1(lines: List<String>): Int {
-        return 0
+        val times = getNumbers(lines.first())
+        val records = getNumbers(lines.last())
+
+        val winningAmounts = mutableListOf<Int>()
+
+        for ((index, time) in times.withIndex()) {
+            val winningHolds = mutableListOf<Int>()
+            for (currentHoldTime in 0..time) {
+                if (currentHoldTime * (time - currentHoldTime) > records[index]) winningHolds.add(currentHoldTime)
+            }
+            if (winningHolds.isNotEmpty()) winningAmounts.add(winningHolds.size)
+        }
+
+
+        return winningAmounts.reduce { acc, number -> acc * number }
+    }
+
+    private fun getNumbers(line: String): List<Int> {
+        val regex = Regex("\\d+")
+        return regex.findAll(line).map { it.value.toInt() }.toList()
     }
 
     @Test
@@ -12,11 +31,11 @@ class Challenge2023Day06 {
         val lines = File("./AdventOfCode/Data/Day06-1-Test-Data.txt").bufferedReader().readLines()
         val exampleSolution1 = solve1(lines)
         println("Example solution 1: $exampleSolution1")
-        assertEquals(13, exampleSolution1)
+        assertEquals(288, exampleSolution1)
 
         val realLines = File("./AdventOfCode/Data/Day06-1-Data.txt").bufferedReader().readLines()
         val solution1 = solve1(realLines)
         println("Solution 1: $solution1")
-        assertEquals(26426, solution1)
+        assertEquals(3316275, solution1)
     }
 }
