83dcf402457843e1e4303f1abf286f5bd5406b95
MartinWie <donotsuspend@googlegroups.com>
2023-12-13 23:58:41 +0100

    Add logical framework in Challenge2023Day07
    The logic for the challenge of day 7 in AdventOfCode is initiated in Kotlin. The approach of determining the value of different characters is set by using a map. The 'solve1' function is now capable of parsing the input lines, determining the current hand and counting the frequency of each number in the hand.

--- AdventOfCode/Challenge2023Day07.kt
+++ AdventOfCode/Challenge2023Day07.kt
@@ -4,6 +4,32 @@ import kotlin.test.assertEquals
 
 class Challenge2023Day07 {
     private fun solve1(lines: List<String>): Int {
+        val valueMap = mapOf(
+            'A' to 14,
+            'K' to 13,
+            'Q' to 12,
+            'J' to 11,
+            'T' to 10,
+            '9' to 9,
+            '8' to 8,
+            '7' to 7,
+            '6' to 6,
+            '5' to 5,
+            '4' to 4,
+            '3' to 3,
+            '2' to 2
+        )
+
+        val (currentCardsRaw, winningAmountRaw) = lines[0].split(" ")
+        val currentCards = currentCardsRaw.map { valueMap[it]!! }
+        val winningAmount = winningAmountRaw.toInt()
+
+        val currentHand = currentCards to winningAmount
+        // check how often a number is in a list of numbers
+        val numberCounts: Map<Int, Int> = currentHand.first.groupingBy { it }.eachCount()
+
+
+
         return 0
     }
 
