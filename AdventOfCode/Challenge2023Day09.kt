b65adefc1f2696143251a00b2b605c6df4d21e91
MartinWie <donotsuspend@googlegroups.com>
2023-12-22 20:47:47 +0100

    Add number prediction method to Challenge2023Day09
    A new method, solve2, has been added to the AdventOfCode Challenge2023Day09. This method predicts and adds the next number in an input sequence based on certain calculations. Corresponding test cases have also been updated to verify the functionality of the newly added method.

--- AdventOfCode/Challenge2023Day09.kt
+++ AdventOfCode/Challenge2023Day09.kt
@@ -52,6 +52,54 @@ class Challenge2023Day09 {
         return results.sum()
     }
 
+    private fun solve2(lines: List<String>): Int {
+        val results = mutableListOf<Int>()
+
+        lines.forEach { line ->
+            // Generate the relevant lines
+            val initialNumbers = line.split(" ").map { it.toInt() }
+            val numberRows = mutableListOf<List<Int>>()
+
+            numberRows.add(initialNumbers)
+
+            var currentRow = numberRows.first()
+
+            while (currentRow.reduce { acc, i -> abs(i) + acc } != 0) {
+                val newRow = mutableListOf<Int>()
+
+                var formerNumber: Int? = null
+                currentRow.forEach {
+                    if (formerNumber != null) {
+                        newRow.add(it - formerNumber!!)
+                    }
+                    formerNumber = it
+                }
+
+                numberRows.add(newRow)
+
+                currentRow = numberRows.last()
+            }
+
+            val numberRowsReversed = numberRows.reversed()
+
+            var currentPredictedNumber = 0
+            var formerRow: MutableList<Int>? = null
+
+            for (row in numberRowsReversed) {
+                val formerPredict = currentPredictedNumber
+                if (formerRow != null) {
+                    currentPredictedNumber = row.first() - formerPredict
+                }
+
+                formerRow = row.toMutableList()
+                formerRow.add(formerPredict)
+            }
+            results.add(currentPredictedNumber)
+        }
+
+        return results.sum()
+    }
+
     @Test
     fun test() {
         val lines = File("./AdventOfCode/Data/Day09-1-Test-Data.txt").bufferedReader().readLines()
@@ -63,5 +111,15 @@ class Challenge2023Day09 {
         val solution1 = solve1(realLines)
         println("Solution 1: $solution1")
         assertEquals(1861775706, solution1)
+
+        val lines2 = File("./AdventOfCode/Data/Day09-1-Test-Data.txt").bufferedReader().readLines()
+        val exampleSolution2 = solve2(lines2)
+        println("Example solution 2: $exampleSolution2")
+        assertEquals(2, exampleSolution2)
+
+        val realLines2 = File("./AdventOfCode/Data/Day09-1-Data.txt").bufferedReader().readLines()
+        val solution2 = solve2(realLines2)
+        println("Solution 2: $solution2")
+        assertEquals(1082, solution2)
     }
 }
