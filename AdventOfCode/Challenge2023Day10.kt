9973172016cc3e958e3b14829f0d44d25e86cfaf
MartinWie <donotsuspend@googlegroups.com>
2023-12-28 01:55:46 +0100

    Refactor solve1 and enhance code readability in Challenge2023Day10.kt
    The solve1 function inside Challenge2023Day10.kt file is refactored to be more readable. A Coord data class is now implemented instead for ease of coordinate tracking. Alongside it, the logic of getting next positions is relocated and improved through a while loop and a new function called findAdjacentCoords. The commit also includes a new test setup, Day10-1-Test-Data2.txt, to increase test coverage.

--- AdventOfCode/Challenge2023Day10.kt
+++ AdventOfCode/Challenge2023Day10.kt
@@ -1,76 +1,87 @@
 import org.junit.Test
 import java.io.File
+import kotlin.math.ceil
 import kotlin.test.assertEquals
 
 class Challenge2023Day10 {
-    private fun solve1(lines: List<String>): Int {
-        // Initializing the map with '.' on every field
-        val map = MutableList(lines.size) {MutableList(lines.size) {'.'} }
+    data class Coord(val row: Int, val col: Int)
+
+    private fun solve1(map: List<String>): Int {
+        val startCoord =
+            map.withIndex().find { (_, line) -> 'S' in line }
+                ?.let { Coord(it.index, it.value.indexOf('S')) }
+
+        var prev: Coord? = null
+        var current = startCoord
         var steps = 0
-        var startingPosition = 0 to 0
 
-        for ((y, line) in lines.withIndex()) {
-            for ((x, char) in line.withIndex()) {
-                if (char == 'S') startingPosition = y to x
-                map[y][x] = char
-            }
-        }
-
-        var currentPosition = startingPosition
-        var forbiddenNext = 'X'
-        while ( steps == 0 || currentPosition != startingPosition ) {
-
-            val result = getNextPosition(currentPosition, map, forbiddenNext)
-            currentPosition = result.first
-            forbiddenNext = result.second
-
+        while (true) {
+            val adjacent = findAdjacentCoords(current!!, map).firstOrNull { it != prev }
             steps++
+
+            if (adjacent == null || adjacent == startCoord) {
+                break
+            }
+
+            prev = current
+            current = adjacent
         }
 
-        return (steps / 2) + if (steps % 2 == 0) 0 else 1
+        return ceil(steps.toDouble() / 2).toInt()
     }
-    private fun getNextPosition(
-        position: Pair<Int, Int>,
-        map: MutableList<MutableList<Char>>,
-        forbiddenNext: Char
-        ): Pair<Pair<Int, Int>, Char> {
 
-        val currentChar = map[position.first][position.second]
-        return when {
-            // Below
-            map[position.first + 1][position.second] in "|LJS" && forbiddenNext != 'S' -> {
-                (position.first + 1 to position.second) to 'N'
+    private fun findAdjacentCoords(
+        coord: Coord,
+        map: List<String>,
+    ): List<Coord> {
+        val adj = mutableListOf<Coord>()
+
+        // Check the coordinate above
+        if (coord.row > 0 && "S|JL".contains(map[coord.row][coord.col])) {
+            if ("|7F".contains(map[coord.row - 1][coord.col])) {
+                adj.add(Coord(coord.row - 1, coord.col))
             }
-
-            // Right
-            map[position.first][position.second + 1] in "-J7S" && forbiddenNext != 'E' -> {
-                (position.first to position.second + 1) to 'W'
-            }
-
-            // Left
-            map[position.first][position.second - 1] in "-LFS" && forbiddenNext != 'W' -> {
-                (position.first to position.second - 1) to 'E'
-            }
-
-            // Above
-            map[position.first - 1][position.second] in "|7FS" && forbiddenNext != 'N' -> {
-                (position.first - 1 to position.second) to 'S'
-            }
-
-            else -> { error("How did we end here?") }
         }
+
+        // Check the coordinate below
+        if (coord.row < map.size - 1 && "S|7F".contains(map[coord.row][coord.col])) {
+            if ("|JL".contains(map[coord.row + 1][coord.col])) {
+                adj.add(Coord(coord.row + 1, coord.col))
+            }
+        }
+
+        // Check the coordinate to the left
+        if (coord.col > 0 && "S-7J".contains(map[coord.row][coord.col])) {
+            if ("-FL".contains(map[coord.row][coord.col - 1])) {
+                adj.add(Coord(coord.row, coord.col - 1))
+            }
+        }
+
+        // Check the coordinate to the right
+        if (coord.col < map[0].length - 1 && "S-LF".contains(map[coord.row][coord.col])) {
+            if ("-J7".contains(map[coord.row][coord.col + 1])) {
+                adj.add(Coord(coord.row, coord.col + 1))
+            }
+        }
+
+        return adj
     }
 
     @Test
     fun test() {
-       /* val lines = File("./AdventOfCode/Data/Day10-1-Test-Data.txt").bufferedReader().readLines()
+        val lines = File("./AdventOfCode/Data/Day10-1-Test-Data.txt").bufferedReader().readLines()
         val exampleSolution1 = solve1(lines)
         println("Example solution 1: $exampleSolution1")
         assertEquals(4, exampleSolution1)
-*/
+
+        val lines2 = File("./AdventOfCode/Data/Day10-1-Test-Data2.txt").bufferedReader().readLines()
+        val exampleSolution2 = solve1(lines2)
+        println("Example solution 1: $exampleSolution2")
+        assertEquals(4, exampleSolution2)
+
         val realLines = File("./AdventOfCode/Data/Day10-1-Data.txt").bufferedReader().readLines()
         val solution1 = solve1(realLines)
         println("Solution 1: $solution1")
-        assertEquals(1861775706, solution1)
+        assertEquals(6701, solution1)
     }
 }
