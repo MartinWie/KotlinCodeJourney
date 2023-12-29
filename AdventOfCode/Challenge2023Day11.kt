2f94f2704b10ca04c71d5c628d109a817de2d280
MartinWie <donotsuspend@googlegroups.com>
2023-12-30 00:33:33 +0100

    Update expected result in test case for Day 11 challenge
    The expected result for the test scenario in the Day 11 challenge of AdventOfCode was updated to reflect the correct solution. This will ensure that the test case accurately verifies the correctness of the challenge solution logic.

--- AdventOfCode/Challenge2023Day11.kt
+++ AdventOfCode/Challenge2023Day11.kt
@@ -13,7 +13,7 @@ class Challenge2023Day11 {
         val lines = File("./AdventOfCode/Data/Day10-1-Test-Data.txt").bufferedReader().readLines()
         val exampleSolution1 = solve1(lines)
         println("Example solution 1: $exampleSolution1")
-        assertEquals(4, exampleSolution1)
+        assertEquals(374, exampleSolution1)
 
         val realLines = File("./AdventOfCode/Data/Day10-1-Data.txt").bufferedReader().readLines()
         val solution1 = solve1(realLines)
