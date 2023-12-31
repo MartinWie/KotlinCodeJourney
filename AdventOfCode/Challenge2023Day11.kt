ddb97488aa8c172e5b5f234c59ed1854986d3f46
MartinWie <donotsuspend@googlegroups.com>
2023-12-31 19:33:24 +0100

    Expand galaxy mapping logic in Challenge2023Day11
    In Challenge2023Day11, expanded the mapping logic to cater to additional galaxy scenarios. This was achieved through introduction of map lines which contain no galaxies, char list generation for the galaxies and placeholder implementation for galaxy numeration and path calculation. The changes enhance the function 'solve1' to handle more complex cases.

--- AdventOfCode/Challenge2023Day11.kt
+++ AdventOfCode/Challenge2023Day11.kt
@@ -3,8 +3,36 @@ import java.io.File
 import kotlin.test.assertEquals
 
 class Challenge2023Day11 {
-
     private fun solve1(lines: List<String>): Int {
+        var currentGalaxyAmount = 0
+        val map = mutableListOf<String>()
+        // First lets expand the map
+        for (line in lines) {
+            map.add(line)
+            // If the line does not contain any galaxies we can conviniently use it to add it to the expanded map
+            if (line.replace(".", "").isEmpty()) map.add(line)
+        }
+
+        val finalMap = mutableListOf<String>()
+        for (i in map.indices) {
+            val tmpCharList = mutableListOf<Char>()
+            for (j in lines.indices) {
+                val currentChar = map[i][j]
+                if (currentChar == '#') {
+                    tmpCharList.add(currentChar)
+                }
+            }
+            if (tmpCharList.isNotEmpty()) {
+                for (j in lines.indices) {
+                    // TODO: implement expansion for columns
+                }
+            }
+        }
+
+        // Implement Galaxy numeration (use currentGalaxyAmount)
+
+        // Implement path calculation
+
         return lines.size
     }
 
