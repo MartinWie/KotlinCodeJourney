a6c9f761d7c46d0d3707d0b9dab6a52ad42c08d9
MartinWie <donotsuspend@googlegroups.com>
2024-02-08 22:42:16 +0100

    Implement vertical rock movement logic and add helper extension function + tests

--- AdventOfCode/Challenge2023Day14.kt
+++ AdventOfCode/Challenge2023Day14.kt
@@ -1,9 +1,31 @@
+import org.junit.Test
 import java.io.File
 import kotlin.test.assertEquals
 
 class Challenge2023Day14 {
     private fun solve1(lines: List<String>): Int {
+        val map = mutableListOf<String>()
+        map.addAll(lines)
+
         var resultLoad = 0
+        val verticalMax = map.indices.last
+
+        for (rowNum in 0..verticalMax) {
+            var verticalPointer = 0
+            var validVerticalPosition = 0
+
+            while (verticalPointer < verticalMax) {
+                val currentChar = map[rowNum][verticalPointer]
+                when (currentChar) {
+                    '0' -> {
+                        map[rowNum][verticalPointer] = '.'
+                    } // TODO: finish implementation
+
+                    else -> throw IllegalStateException("Illegal char!")
+                }
+                verticalPointer++
+            }
+        }
 
         // TODO: go through each line starting at 1 and see if there is a round rock
         // Found rock -> move up until line 0 or encountered other rock.
@@ -15,6 +37,7 @@ class Challenge2023Day14 {
         return resultLoad
     }
 
+    @Test
     fun test() {
         val lines = File("./AdventOfCode/Data/Day14-1-Test-Data.txt").bufferedReader().readLines()
         val exampleSolution1 = solve1(lines)
@@ -26,4 +49,21 @@ class Challenge2023Day14 {
         println("Solution 1: $solution1")
         assertEquals(9623138, solution1)
     }
+
+    @Test
+    fun `Helper Method test`() {
+        val testString = "123456789"
+        val result = testString.set(1, '0')
+        assertEquals("103456789", result)
+
+        val result2 = testString.set(0, '0')
+        assertEquals("023456789", result2)
+
+        val result3 = testString.set(testString.lastIndex, '0')
+        assertEquals("123456780", result3)
+    }
+}
+
+private operator fun String.set(replacementIndex: Int, value: Char): String {
+    return this.take(replacementIndex) + value + this.drop(replacementIndex + 1)
 }
