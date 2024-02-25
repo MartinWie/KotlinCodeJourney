8ea837f637cf1350dc625d9690435653360992fa
MartinWie <donotsuspend@googlegroups.com>
2024-02-25 21:55:45 +0100

    Update Challenge2023Day17 with notes about weighted graph approach and test values.

--- AdventOfCode/Challenge2023Day17.kt
+++ AdventOfCode/Challenge2023Day17.kt
@@ -5,6 +5,10 @@ import kotlin.test.assertEquals
 class Challenge2023Day17 {
 
     private fun solve1(lines: List<String>): Int {
+        // Looks a lot like a weighted graph traversal
+        // TODO: brush up on weighted graph knowledge
+        // (probably Dijkstra with some minor mod (max 3 steps in same direction))
+
         return lines.size
     }
 
@@ -13,11 +17,11 @@ class Challenge2023Day17 {
         val lines = File("./AdventOfCode/Data/Day17-1-Test-Data.txt").bufferedReader().readLines()
         val exampleSolution1 = solve1(lines)
         println("Example solution 1: $exampleSolution1")
-        assertEquals(46, exampleSolution1)
+        assertEquals(102, exampleSolution1)
 
         val realLines = File("./AdventOfCode/Data/Day17-1-Data.txt").bufferedReader().readLines()
         val solution1 = solve1(realLines)
         println("Solution 1: $solution1")
-        assertEquals(7074, solution1)
+        assertEquals(1, solution1)
     }
 }
