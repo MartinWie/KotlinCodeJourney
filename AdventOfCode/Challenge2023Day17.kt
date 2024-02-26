c87c4f72038463c96c82cf4cd20df40613ad3fc5
MartinWie <donotsuspend@googlegroups.com>
2024-02-26 22:39:11 +0100

    Implement basic pathfinding setup with node costs in Challenge17.

--- AdventOfCode/Challenge2023Day17.kt
+++ AdventOfCode/Challenge2023Day17.kt
@@ -9,6 +9,25 @@ class Challenge2023Day17 {
         // TODO: brush up on weighted graph knowledge
         // (probably Dijkstra with some minor mod (max 3 steps in same direction))
 
+        val start = Pair(0, 0)
+        val end = Pair(lines.first().lastIndex, lines.lastIndex)
+
+        val visited = mutableListOf<Pair<Int, Int>>()
+        val nodes = mutableMapOf<Pair<Int, Int>, Int>()
+        val travelCosts = mutableMapOf<Pair<Int, Int>, Int>()
+
+        for (y in lines.indices) {
+            for (x in lines.first().indices) {
+                nodes[Pair(x, y)] = lines[y][x].digitToInt()
+                travelCosts[Pair(x, y)] = 999999
+            }
+        }
+
+        visited.add(start)
+        travelCosts[start] = 0
+
+        // TODO: finish implementation(do not forget to implement constraint(max 3 in same direction))
+
         return lines.size
     }
 
