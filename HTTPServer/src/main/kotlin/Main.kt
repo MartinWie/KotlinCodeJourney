1b49a14015ee2e00257f4bb5825eed8eeb1ca376
MartinWie <donotsuspend@googlegroups.com>
2024-03-03 22:31:34 +0100

    Implement simple TCP echo server in Main.kt

--- HTTPServer/src/main/kotlin/Main.kt
+++ HTTPServer/src/main/kotlin/Main.kt
@@ -1,5 +1,30 @@
 package de.mw
 
+import java.io.BufferedReader
+import java.io.InputStreamReader
+import java.io.PrintWriter
+import java.net.ServerSocket
+
 fun main() {
-    println("Hello World!")
+    println("Starting simple TCP server!")
+
+    val port = 8888
+
+    val serverSocket = ServerSocket(port)
+    println("Listening on port: $port")
+
+    while (true) {
+        val clientSocket = serverSocket.accept()
+        println("New connection: ${clientSocket.inetAddress}")
+
+        val input = BufferedReader(InputStreamReader(clientSocket.getInputStream()))
+        val output = PrintWriter(clientSocket.getOutputStream(), true)
+
+        val clientMessage = input.readLines()
+        println("Message form client: $clientMessage")
+
+        output.println(clientMessage)
+
+        clientSocket.close()
+    }
 }
