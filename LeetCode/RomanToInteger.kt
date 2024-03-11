1a0456148e9154bae93384864851892164976ec9
MartinWie <donotsuspend@googlegroups.com>
2024-03-11 23:58:47 +0100

    Refactor RomanToInteger to enhance readability and add tests
    - Change the visibility of romanToInt method from public to private
    - Implement an alternative romanToInt2 method for comparison purposes
    - Add additional test cases to cover more scenarios in test2 method

--- LeetCode/RomanToInteger.kt
+++ LeetCode/RomanToInteger.kt
@@ -2,7 +2,7 @@ import org.junit.Test
 import kotlin.test.assertEquals
 
 class RomanToInteger {
-    fun romanToInt(s: String): Int {
+    private fun romanToInt(s: String): Int {
         val letterToValMap = hashMapOf(
             'I' to 1,
             'V' to 5,
@@ -30,10 +30,51 @@ class RomanToInteger {
         return result
     }
 
+    private fun romanToInt2(s: String): Int {
+        val letterToValMap = hashMapOf(
+            'I' to 1,
+            'V' to 5,
+            'X' to 10,
+            'L' to 50,
+            'C' to 100,
+            'D' to 500,
+            'M' to 1000
+        )
+
+        var result = 0
+        var currentPointer: Int = s.lastIndex
+        var nextPointer: Int = currentPointer - 1
+
+        while (nextPointer > -1) {
+            val current = letterToValMap[s[currentPointer]]!!
+            val next = letterToValMap[s[nextPointer]]!!
+            result = if (next < current) {
+                result + (current * -1)
+            } else {
+                result + current
+            }
+
+            currentPointer--
+            nextPointer--
+        }
+
+        result += letterToValMap[s.first()]!!
+
+        return result
+    }
+
     @Test
     fun test1() {
         assertEquals(3, romanToInt("III"))
         assertEquals(58, romanToInt("LVIII"))
         assertEquals(1994, romanToInt("MCMXCIV"))
+    }
+
+    @Test
+    fun test2() {
+        assertEquals(3, romanToInt2("III"))
+        assertEquals(58, romanToInt2("LVIII"))
+        assertEquals(9, romanToInt2("IX"))
+        assertEquals(1994, romanToInt2("MCMXCIV"))
     }
 }
