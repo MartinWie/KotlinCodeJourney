d5362f70b553b7f4c118d017dea9b380c5183637
MartinWie <donotsuspend@googlegroups.com>
2024-03-18 18:18:54 +0100

    refactor: simplify duplicate removal logic in RemoveDuplicatesFromSortedArray
    - Optimize the solution by implementing a two-pointer approach.
    - Remove unused imports and test functions.
    - Ensure the method handles empty input arrays correctly.

--- LeetCode/RemoveDuplicatesFromSortedArray.kt
+++ LeetCode/RemoveDuplicatesFromSortedArray.kt
@@ -1,33 +1,15 @@
-import org.junit.Test
-import kotlin.test.assertEquals
-
 class RemoveDuplicatesFromSortedArray {
     private fun solve(nums: IntArray): Int {
-        var counter = 0
-        var formerNum = nums.first() + 1
-        for (i in 0..nums.lastIndex) {
-            if (nums[i] == formerNum) {
-                nums.removeAt(i)
-                counter++
+        if (nums.isEmpty()) return 0
+
+        var i = 0
+        for (j in 1..nums.lastIndex) {
+            if (nums[i] != nums[j]) {
+                i++
+                nums[i] = nums[j]
             }
         }
 
-        return nums.size
-    }
-
-    private fun IntArray.removeAt(index: Int) {
-        for (i in index until this.lastIndex) {
-            this[i] = this[i + 1]
-        }
-        this[this.lastIndex] = 0
-    }
-
-    @Test
-    fun testIntArrayExtensionFunction() {
-        val arr = intArrayOf(1, 2, 3, 4)
-        arr.removeAt(0)
-        arr.removeAt(0)
-
-        assertEquals(3, arr.first())
+        return i + 1
     }
 }
