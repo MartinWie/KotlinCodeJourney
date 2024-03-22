74754112c9ed2bf632e376ac1f75e9c637f3740b
MartinWie <donotsuspend@googlegroups.com>
2024-03-22 22:00:41 +0100

    Added logic to detect longest palindromic substring and handle single character edge case.

--- LeetCode/LongestPalindromicSubstring.kt
+++ LeetCode/LongestPalindromicSubstring.kt
@@ -5,14 +5,22 @@ import kotlin.test.assertTrue
 
 class LongestPalindromicSubstring {
     private fun solve(s: String): String {
+        if (s.length == 1) return s
+
         var currentLongestPalindrome = ""
-        var currentPointer = 0
-        var tmpPalindrome = ""
+
         for (i in 0..s.lastIndex) {
-            // TODO: finish implementation of solve
-            if (s.lastIndex - i <= currentLongestPalindrome.length) break
+            for (j in i..s.lastIndex) {
+                val current = s.substring(i, j)
+                if (isPalindrom(current) && current.length > currentLongestPalindrome.length) {
+                    currentLongestPalindrome = current
+                }
+            }
+            if (s.lastIndex - i <= currentLongestPalindrome.length) {
+                break
+            }
         }
-        return "TBD"
+        return currentLongestPalindrome
     }
 
     private fun isPalindrom(s: String): Boolean {
@@ -30,8 +38,10 @@ class LongestPalindromicSubstring {
 
     @Test
     fun `Test solve`() {
+        assertEquals("bb", solve("bb"))
         assertEquals("bab", solve("babad"))
         assertEquals("bb", solve("cbbd"))
+        assertEquals("a", solve("a"))
     }
 
     @Test
