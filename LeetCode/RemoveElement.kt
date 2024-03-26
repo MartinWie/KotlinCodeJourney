1ee38d78d821fae44af14ad6423392028e086f11
MartinWie <donotsuspend@googlegroups.com>
2024-03-26 21:28:16 +0100

    Implemented RemoveElement logic to exclude specific values and return the new length, with tests updated accordingly.

--- LeetCode/RemoveElement.kt
+++ LeetCode/RemoveElement.kt
@@ -2,12 +2,20 @@ import org.junit.Test
 import kotlin.test.assertEquals
 
 class RemoveElement {
-    fun removeElement(nums: IntArray, `val`: Int): Int {
-        return nums.size
+    fun solve1(nums: IntArray, `val`: Int): Int {
+        var i = 0
+        for (j in 0..nums.lastIndex) {
+            if (nums[j] != `val`) {
+                nums[i] = nums[j]
+                i++
+            }
+        }
+
+        return i
     }
 
     @Test
     fun test() {
-        assertEquals(5, removeElement(intArrayOf(0, 1, 2, 2, 3, 0, 4, 2), 2))
+        assertEquals(5, solve1(intArrayOf(0, 1, 2, 2, 3, 0, 4, 2), 2))
     }
 }
