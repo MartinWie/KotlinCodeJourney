8e6fb210f98140430aa35b2657aa04d38f733eda
MartinWie <donotsuspend@googlegroups.com>
2024-03-28 20:44:47 +0100

    Refined binary search logic in SearchInsertPosition and expanded test suite, next edge case.

--- LeetCode/SearchInsertPosition.kt
+++ LeetCode/SearchInsertPosition.kt
@@ -6,12 +6,12 @@ class SearchInsertPosition {
         var leftPointer = 0
         var rightPoiter = nums.lastIndex
 
-        while (leftPointer < rightPoiter - 1) {
+        while (leftPointer < rightPoiter) {
             val middlePointer = (leftPointer + rightPoiter) / 2
             when {
                 nums[middlePointer] == target -> return middlePointer
-                nums[middlePointer] < target -> leftPointer = middlePointer
-                nums[middlePointer] > target -> rightPoiter = middlePointer
+                nums[middlePointer] < target -> leftPointer = middlePointer + 1
+                nums[middlePointer] > target -> rightPoiter = middlePointer - 1
             }
         }
 
@@ -24,5 +24,6 @@ class SearchInsertPosition {
         assertEquals(2, solve1(nums = intArrayOf(1, 3, 4, 5, 6), target = 4))
         assertEquals(1, solve1(nums = intArrayOf(1, 3, 5, 6), target = 2))
         assertEquals(4, solve1(nums = intArrayOf(1, 3, 5, 6), target = 7))
+        assertEquals(0, solve1(nums = intArrayOf(1, 3, 5, 6), target = 0))
     }
 }
