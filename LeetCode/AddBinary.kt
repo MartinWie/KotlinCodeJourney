6b4e89012e397adf703c3b1b2a7180be8ee0422d
MartinWie <donotsuspend@googlegroups.com>
2024-04-01 23:47:52 +0200

    Started implementation for 'solve' method in AddBinary class and corresponding tests.

--- LeetCode/AddBinary.kt
+++ LeetCode/AddBinary.kt
@@ -1,9 +1,29 @@
 import org.junit.Test
+import java.util.*
 import kotlin.test.assertEquals
 
 class AddBinary {
     private fun solve(a: String, b: String): String {
-        return ""
+        var result = ""
+        var carryOver = 0
+
+        val aStack = Stack<Char>()
+        val bStack = Stack<Char>()
+
+        a.forEach { aStack.add(it) }
+        b.forEach { bStack.add(it) }
+
+        while (a.isNotEmpty() || b.isNotEmpty()) {
+            val aTemp = aStack.pop()
+            val bTemp = bStack.pop()
+
+            when {
+                aTemp == '1' && bTemp == '1' -> {}
+                // TODO: think about this and then implement efficient solution
+            }
+        }
+
+        return result.reversed()
     }
 
     @Test
