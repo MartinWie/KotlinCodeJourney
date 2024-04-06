cfd5073311833bf8bd7ee52338c72e98fc5938c2
MartinWie <donotsuspend@googlegroups.com>
2024-04-06 23:31:58 +0200

    Initialized new Kotlin module 'HTMXTest' with JVM toolchain and JUnit support.

---
+++ HTMXTest/build.gradle.kts
@@ -0,0 +1,21 @@
+plugins {
+    kotlin("jvm")
+}
+
+group = "de.mw"
+version = "0.0.1"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation("org.jetbrains.kotlin:kotlin-test")
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
+kotlin {
+    jvmToolchain(17)
+}
