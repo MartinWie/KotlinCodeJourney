b56356b158a99a9aa0fafbdcdc0d3a8c8c33b07d
MartinWie <donotsuspend@googlegroups.com>
2024-04-07 00:00:00 +0200

    Initialized Ktor server setup with dependencies, configured application plugin, and added logging in Kotlin-HTMX-Test module.

--- HTMXTest/build.gradle.kts
+++ HTMXTest/build.gradle.kts
@@ -1,15 +1,54 @@
+val ktor_version: String by project
+val kotlin_version: String by project
+val logback_version: String by project
+
+
 plugins {
     kotlin("jvm")
+    id("io.ktor.plugin") version "2.3.3"
 }
 
 group = "de.mw"
 version = "0.0.1"
+val mainClassString = "de.mw.MainKt"
+application {
+    mainClass.set(mainClassString)
+}
 
 repositories {
     mavenCentral()
+    maven { url = uri("https://maven.pkg.jetbrains.space/kotlin/p/kotlin/kotlin-js-wrappers") }
 }
 
 dependencies {
+    implementation("io.ktor:ktor-server-core-jvm:$ktor_version")
+    implementation("io.ktor:ktor-server-host-common-jvm:$ktor_version")
+    implementation("io.ktor:ktor-server-status-pages-jvm:$ktor_version")
+    implementation("io.ktor:ktor-server-cors-jvm:$ktor_version")
+    implementation("io.ktor:ktor-server-default-headers-jvm:$ktor_version")
+    implementation("io.ktor:ktor-server-forwarded-header-jvm:$ktor_version")
+    implementation("io.ktor:ktor-server-call-logging-jvm:$ktor_version")
+    implementation("io.ktor:ktor-server-call-id-jvm:$ktor_version")
+    implementation("io.ktor:ktor-server-content-negotiation-jvm:$ktor_version")
+    implementation("io.ktor:ktor-serialization-jackson-jvm:$ktor_version")
+    implementation("io.ktor:ktor-server-caching-headers:$ktor_version")
+    implementation("io.ktor:ktor-server-netty-jvm:$ktor_version")
+    implementation("io.ktor:ktor-server-caching-headers-jvm:$ktor_version")
+    implementation("io.ktor:ktor-server-html-builder-jvm:$ktor_version")
+
+    implementation("io.ktor:ktor-client-core:$ktor_version")
+    implementation("io.ktor:ktor-client-cio:$ktor_version")
+    implementation("io.ktor:ktor-client-content-negotiation:$ktor_version")
+
+    implementation("org.jetbrains:kotlin-css-jvm:1.0.0-pre.156-kotlin-1.5.0")
+
+    implementation("ch.qos.logback:logback-classic:$logback_version")
+    implementation("com.nfeld.jsonpathkt:jsonpathkt:2.0.1")
+    implementation("com.fasterxml.jackson.datatype:jackson-datatype-jsr310:2.15.2")
+
+    testImplementation("org.assertj:assertj-core:3.24.2")
+    testImplementation("io.ktor:ktor-server-tests-jvm:$ktor_version")
+    testImplementation("org.jetbrains.kotlin:kotlin-test-junit:$kotlin_version")
     testImplementation("org.jetbrains.kotlin:kotlin-test")
 }
 
