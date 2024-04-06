73d916a8a270f0c869b7bf7f1e5c7edbbd5d95bc
MartinWie <donotsuspend@googlegroups.com>
2024-04-06 23:38:40 +0200

    Added project todo list in Main.kt for the Kotlin-HTMX-Test module setup.

--- HTMXTest/src/main/kotlin/Main.kt
+++ HTMXTest/src/main/kotlin/Main.kt
@@ -3,3 +3,11 @@ package de.mw
 fun main() {
     println("Hello World!")
 }
+
+/* Project todos:
+    - Get ktor to work
+    - First HTMX ure
+    - Add tailwind
+    - Implement mini use case with in memory state
+    - Look into jOOQ ?
+ */
