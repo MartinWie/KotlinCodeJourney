21b339de98c40078f432b992965e32788624bde9
MartinWie <donotsuspend@googlegroups.com>
2024-04-08 22:14:58 +0200

    Tested kotlin html DSL and did first HTMX test

--- HTMXTest/src/main/kotlin/plugins/Routing.kt
+++ HTMXTest/src/main/kotlin/plugins/Routing.kt
@@ -4,23 +4,33 @@ import io.ktor.http.*
 import io.ktor.server.application.*
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
+import kotlinx.html.*
+import kotlinx.html.stream.createHTML
 
 fun Application.configureRouting() {
     routing {
         get("/") {
-            val htmlContent = """
-                <!DOCTYPE html>
-                <html>
-                <head>
-                    <title>My Web Page</title>
-                </head>
-                <body>
-                    <h1>Welcome to Ktor!</h1>
-                    <p>This is an example of serving HTML with Ktor.</p>
-                </body>
-                </html>
-            """.trimIndent()
+            val htmlContent = "<!DOCTYPE html>" + createHTML().html {
+                head {
+                    title = "My Web Page"
+                    script { src = "https://unpkg.com/htmx.org@1.9.11" }
+                }
+                body {
+                    h1 { +"Welcome to my first htmx page!" }
+                    p { +"Wow much p tag :D" }
+                    button {
+                        attributes["hx-post"] = "/clicked"
+                        attributes["hx-swap"] = "outerHTML"
+                        +"Click Me"
+                    }
+                }
+            }
 
+            call.respondText(htmlContent, ContentType.Text.Html)
+        }
+
+        post("/clicked") {
+            val htmlContent = "Test"
             call.respondText(htmlContent, ContentType.Text.Html)
         }
     }
