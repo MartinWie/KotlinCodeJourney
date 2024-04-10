3c18b33333a58c150525ba20486d781c418fcdc1
MartinWie <donotsuspend@googlegroups.com>
2024-04-10 22:47:36 +0200

    Added hxGet function and updated documentation and parameter naming in hxPost and hxSwap functions in htmxAttributes.kt.

--- HTMXTest/src/main/kotlin/utils/htmxAttributes.kt
+++ HTMXTest/src/main/kotlin/utils/htmxAttributes.kt
@@ -2,19 +2,30 @@ package de.mw.utils
 
 import kotlinx.html.HTMLTag
 
+
+/**
+ * The hx-get attribute will cause an element to issue a GET to the specified URL
+ * and swap the HTML into the DOM using a swap strategy.
+ *
+ * https://htmx.org/attributes/hx-get/
+ */
+fun HTMLTag.hxGet(path: String) {
+    attributes += "hx-get" to path
+}
+
 /**
  * The hx-post attribute will cause an element to issue a POST to the specified URL
- * and swap the HTML into the DOM using a swap strategy.
+ * and swap the HTML into the DOM using a swap strategy(HxSwapOption).
  *
  * https://htmx.org/attributes/hx-post/
  */
-fun HTMLTag.hxPost(value: String) {
-    attributes += "hx-post" to value
+fun HTMLTag.hxPost(path: String) {
+    attributes += "hx-post" to path
 }
 
 /**
  * The hx-swap attribute allows you to specify how the response will be swapped in relative to the target of an AJAX request.
- * If you do not specify the option, the default is htmx.config.defaultSwapStyle (innerHTML).
+ * If you do not specify the option(HxSwapOption), the default is htmx.config.defaultSwapStyle (innerHTML).
  *
  * https://htmx.org/attributes/hx-swap/
  */
