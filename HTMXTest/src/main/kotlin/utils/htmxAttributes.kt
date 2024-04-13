a356cf02ef43480409674e48a1ee9bc19a6e9337
MartinWie <donotsuspend@googlegroups.com>
2024-04-13 13:59:31 +0200

    Introduced `hxDisable` function to disable htmx processing

--- HTMXTest/src/main/kotlin/utils/htmxAttributes.kt
+++ HTMXTest/src/main/kotlin/utils/htmxAttributes.kt
@@ -218,3 +218,17 @@ fun HTMLTag.hxVals(json: String) {
 fun HTMLTag.hxDisable() {
     attributes += "hx-disable" to "true"
 }
+
+/**
+ * The hx-disabled-elt attribute allows you to specify elements
+ * that will have the disabled attribute added to them for the duration of the request.
+ *
+ * The value of this attribute is a CSS query selector of the element or elements to apply the class to,
+ * or the keyword closest, followed by a CSS selector, which will find the closest ancestor element or itself,
+ * that matches the given CSS selector (e.g. "closest tr"), or the keyword "this".
+ *
+ * [Details](https://htmx.org/attributes/hx-disabled-elt/)
+ */
+fun HTMLTag.hxDisabled(selector: String = "this") {
+    attributes += "hx-disabled-elt" to selector
+}
