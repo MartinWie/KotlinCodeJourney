e51b586b17afdb9b4a634ffcc1a221f5acd2e44e
MartinWie <donotsuspend@googlegroups.com>
2024-04-17 22:56:24 +0200

    Added hxSync attribute and associated SyncModifier enum to enhance AJAX request synchronization in HTMX utility functions.

--- HTMXTest/src/main/kotlin/utils/htmxAttributes.kt
+++ HTMXTest/src/main/kotlin/utils/htmxAttributes.kt
@@ -380,3 +380,35 @@ fun HTMLTag.hxPreserve() {
 fun HTMLTag.hxInclude(selector: String = "this") {
     attributes += "hx-include" to selector
 }
+
+/**
+ * The hx-sync attribute allows you to synchronize AJAX requests between multiple elements.
+ *
+ * The hx-sync attribute consists of a CSS selector to indicate the element to synchronize on,
+ * followed optionally by a colon and then by an optional syncing strategy. The available strategies are:
+ *
+ * "drop" - drop (ignore) this request if an existing request is in flight (the default)
+ * "abort" - drop (ignore) this request if an existing request is in flight, and, if that is not the case, abort this request if another request occurs while it is still in flight
+ * "replace" - abort the current request, if any, and replace it with this request
+ * "queue" - place this request in the request queue associated with the given element
+ * The "queue" modifier can take an additional argument indicating exactly how to queue:
+ *
+ * queue first - queue the first request to show up while a request is in flight
+ * queue last - queue the last request to show up while a request is in flight
+ * queue all - queue all requests that show up while a request is in flight
+ *
+ *
+ * [Details](https://htmx.org/attributes/hx-sync/)
+ */
+fun HTMLTag.hxSync(selector: String, modifier: SyncModifier = SyncModifier.DROP) {
+    attributes += "hx-sync" to "$selector :${modifier.value}"
+}
+
+enum class SyncModifier(val value: String) {
+    DROP("drop"),
+    ABORT("abort"),
+    REPLACE("replace"),
+    QUEUE_FIRST("queue first"),
+    QUEUE_LAST("queue last"),
+    QUEUE_ALL("queue all")
+}
