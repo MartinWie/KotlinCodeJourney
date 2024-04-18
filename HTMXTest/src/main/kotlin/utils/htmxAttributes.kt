ff2d274dee78ef15b5ffc1249a3d60b3821db0da
MartinWie <donotsuspend@googlegroups.com>
2024-04-18 23:29:22 +0200

    - Added hxTrigger attribute support to enhance AJAX request customization in HTMX utility.

--- HTMXTest/src/main/kotlin/utils/htmxAttributes.kt
+++ HTMXTest/src/main/kotlin/utils/htmxAttributes.kt
@@ -518,3 +518,17 @@ fun HTMLTag.hxReplaceUrl(useFetchedUrl: Boolean) {
 fun HTMLTag.hxReplaceUrl(url: String) {
     attributes += "hx-replace-url" to url
 }
+
+/**
+ * The hx-trigger attribute allows you to specify what triggers an AJAX request.
+ * A trigger value can be one of the following:
+ *
+ * An event name (e.g. “click” or “my-custom-event”) followed by an event filter and a set of event modifiers
+ * A polling definition of the form "every <timing declaration>"
+ * A comma-separated list of such events
+ *
+ * [Details](https://htmx.org/attributes/hx-trigger/)
+ */
+fun HTMLTag.hxTrigger(trigger: String) {
+    attributes += "hx-trigger" to trigger
+}
